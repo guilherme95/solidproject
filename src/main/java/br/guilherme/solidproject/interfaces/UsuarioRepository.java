3239b48694c61b6388e6f1f3c665a5f981866ad5
mello <guilherme.victor@portoseguro.com.br>
2025-12-23 14:40:24 -0300

    feat: configure leitura de nao bean para repositorio

--- src/main/java/br/guilherme/solidproject/interfaces/UsuarioRepository.java
+++ src/main/java/br/guilherme/solidproject/interfaces/UsuarioRepository.java
@@ -2,9 +2,11 @@ package br.guilherme.solidproject.interfaces;
 
 import br.guilherme.solidproject.domain.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.Optional;
 
+@NoRepositoryBean
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByNome(String nome);
 }
