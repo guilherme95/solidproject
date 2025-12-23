a4962045923c0fb57fed72cc748b0527195a4e8d
mello <guilherme.victor@portoseguro.com.br>
2025-12-23 14:49:14 -0300

    feat: reescreva metodo para atender o principio lsp do solid

--- src/main/java/br/guilherme/solidproject/repository/JpaUsuarioRepository.java
+++ src/main/java/br/guilherme/solidproject/repository/JpaUsuarioRepository.java
@@ -1,10 +1,25 @@
 package br.guilherme.solidproject.repository;
 
+import br.guilherme.solidproject.domain.entity.Usuario;
 import br.guilherme.solidproject.interfaces.UsuarioRepository;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @Profile("jpaRepo")
 public interface JpaUsuarioRepository extends UsuarioRepository {
+
+    @Override
+    @Query("""
+            SELECT
+             usuario
+            FROM
+             Usuario usuario
+            WHERE
+             usuario.nome = :nome
+            """)
+    Optional<Usuario> findByNome(String nome);
 }
--- src/main/java/br/guilherme/solidproject/repository/LegadoUsuarioRepository.java
+++ src/main/java/br/guilherme/solidproject/repository/LegadoUsuarioRepository.java
@@ -1,10 +1,25 @@
 package br.guilherme.solidproject.repository;
 
+import br.guilherme.solidproject.domain.entity.Usuario;
 import br.guilherme.solidproject.interfaces.UsuarioRepository;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @Profile("legacyRepo")
 public interface LegadoUsuarioRepository extends UsuarioRepository {
+
+    @Override
+    @Query(value = """
+            SELECT
+             usuario.*
+            FROM
+             usuario usuario
+            WHERE
+             usuario.nome = :nome
+            """, nativeQuery = true)
+    Optional<Usuario> findByNome(String nome);
 }
